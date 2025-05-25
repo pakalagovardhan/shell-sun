e0c1d27d93b42dff47580574c8fb4ec16ae7e0d1
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-25 12:35:27 +0530

    added some lines

--- 08-install-commands.sh
+++ 08-install-commands.sh
@@ -4,10 +4,17 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]
 then
-    echo "ERROR::: Please run with root access"
+    echo "ERROR:: Please run with root access"
     exit 1
 else
     echo "your running with root access"
 fi
 
 dnf install mysql -y
+
+if [ $? eq 0 ]
+then
+    echo "installing mysql is success...."
+else
+    echo "installing mysql is failed..."
+fi
