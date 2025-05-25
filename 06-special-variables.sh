af8c1db6d4b3e86270be8c021560897cf923a370
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-25 12:01:24 +0530

    added some lines

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,3 +3,9 @@
 echo "All varialbles passed to script:: $@"
 echo "Number of variables:: $#"
 echo "Script name:: $0"
+echo "Current directory:: $PWD"
+echo "User ruuning script:: $USER"
+echo "Home directory:: $HOME"
+echo "PID of the script:: $$"
+sleep 10 &
+echo "PID of last command in background:: $!"
