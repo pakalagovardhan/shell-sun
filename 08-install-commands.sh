1788fa5b873e66fd8157f11782a5be7d4a93d598
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-25 12:49:50 +0530

    added some lines

--- 08-install-commands.sh
+++ 08-install-commands.sh
@@ -10,12 +10,20 @@ else
     echo "your running with root access"
 fi
 
-dnf install mysql -y
+dnf list installed mysql
 
-if [ $? -eq 0 ]
-then
-    echo "installing mysql is success...."
+if [ $? -ne 0 ]
+then 
+    echo "Mysql is not installed going to install it..."
+    dnf install mysql -y
+
+    if [ $? -eq 0 ]
+    then
+        echo "installing mysql is success...."
+    else
+        echo "installing mysql is failed..."
+        exit 1
+    fi
 else
-    echo "installing mysql is failed..."
-    exit 1
+    echo "Mysql is already installed"
 fi
