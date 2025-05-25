42cd2490371c3d65da0314d7a22366c6eb5519ef
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-25 11:25:00 +0530

    added some lines

--- 05-array.sh
+++ 05-array.sh
@@ -2,6 +2,6 @@
 
 ARR=("ram" "gopi" "ravi" "kantha" "giri")
 
-echo "first line $ARR[0]"
-echo "second line $ARR[1]"
-echo "All $ARR[@]"
+echo "first line ${ARR[0]}"
+echo "second line ${ARR[1]}"
+echo "All ${ARR[@]}"
